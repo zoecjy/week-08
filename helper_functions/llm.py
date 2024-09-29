63b1a12ac64384b46862b58a9cfedf03db5ea9ac
zoecjy <zoe.chaijy@gmail.com>
2024-09-29 20:40:45 +0800

    Update

--- helper_functions/llm.py
+++ helper_functions/llm.py
@@ -1,13 +1,17 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 from openai import OpenAI
 import tiktoken
 
-
-load_dotenv('.env')
+if load_dotenv('.env'):
+   # for local development
+   OPENAI_KEY = os.getenv('OPENAI_API_KEY')
+else:
+   OPENAI_KEY = st.secrets['OPENAI_API_KEY']
 
 # Pass the API Key to the OpenAI Client
-client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+client = OpenAI(api_key=OPENAI_KEY)
 
 def get_embedding(input, model='text-embedding-3-small'):
     response = client.embeddings.create(
